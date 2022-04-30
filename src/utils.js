b6f34a40aee3ea529925b1df623d7a607f79d29d
Andrey Takhtamirov <andrey1229@icloud.com>
2022-04-30 02:24:49 -0400

    Add more helper functions

--- src/utils.js
+++ src/utils.js
@@ -6,4 +6,23 @@ export default class Utils {
     static randomInteger(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    static getWidth(mesh) {
+        let boundingBox = mesh.geometry.boundingBox;
+        return boundingBox.max.x - boundingBox.min.x;
+    }
+
+    static getHeight(mesh) {
+        let boundingBox = mesh.geometry.boundingBox;
+        return boundingBox.max.z - boundingBox.min.z;
+    }
+
+    static getDepth(mesh) {
+        let boundingBox = mesh.geometry.boundingBox;
+        return boundingBox.max.y - boundingBox.min.y;
+    }
+
+    static getRadians(degrees) {
+        return degrees * (Math.PI / 180);
+    }
 }
